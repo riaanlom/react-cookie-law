f44e890b167ca8e4313551bac58b03dbd9f9256f
Riaan <riaan@lominctech.com>
2020-02-25 17:14:58 +0200

    .

--- src/components/CookieBannerContent.js
+++ src/components/CookieBannerContent.js
@@ -42,7 +42,7 @@ export default (props = {}) => {
   const cookieOptionStyle = { optionWrapperStyle, optionLabelStyle, checkboxStyle };
 
   return (
-    <div className={`react-cookie-law-dialog ${className}`} style={dialogStyle}>
+    <div className={`react-cookie-law-dialog custom-dialog ${className}`} style={dialogStyle}>
       <div className="react-cookie-law-container" style={containerStyle}>
         <div className="react-cookie-law-msg" style={messageStyle}>{message}</div>
 
